1addbf1b8f624d33cc821e3b54e3bf95afe80c57
gdj84-archive <lecturekbr@naver.com>
2024-10-14 17:05:08 +0900

    Initial commit

---
+++ 01_Spring_IoC_DI/src/main/java/com/br/spring/ioc/xml/Calculator.java
@@ -0,0 +1,13 @@
+package com.br.spring.ioc.xml;
+
+public class Calculator {
+
+	public void plus(int x, int y) {
+		System.out.printf("%d + %d = %d\n", x, y, x+y);
+	}
+	
+	public void minus(int x, int y) {
+		System.out.printf("%d - %d = %d\n", x, y, x-y);
+	}
+	
+}

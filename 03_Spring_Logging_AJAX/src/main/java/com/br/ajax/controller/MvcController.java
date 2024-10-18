b7ea6893ea66c70f693d78f5b22b4f5ad2a5a10e
gdj84-archive <lecturekbr@naver.com>
2024-10-18 11:49:41 +0900

    update

--- 03_Spring_Logging_AJAX/src/main/java/com/br/ajax/controller/MvcController.java
+++ 03_Spring_Logging_AJAX/src/main/java/com/br/ajax/controller/MvcController.java
@@ -1,15 +1,24 @@
 package com.br.ajax.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class MvcController {
 	
+	private Logger logger = LoggerFactory.getLogger(MvcController.class);
+	
 	@RequestMapping(value= {"/", "/main.do"})
 	public String mainPage() {
 		
-		//System.out.println("MvcController의 mainPage 메소드 작동됨"); // 출력문은 성능저하를 야기시킴 => 로그 출력 권장
+		// 출력문은 성능저하를 야기시킴 => 로그 출력 권장
+		//System.out.println("MvcController의 mainPage 메소드 작동됨"); 
+		
+		
+		
 		
 		return "main";
 	}
